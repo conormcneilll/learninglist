f0f985efdfc98ee7d6e7df0ed23aa00edc3462d6
conormcneilll <167910183+conormcneilll@users.noreply.github.com>
2024-06-12 09:29:27 +0100

    API .js will run the backend of the website
    this will be the database connection and CRUD requests managements

--- api/api.js
+++ api/api.js
@@ -1,1 +1,36 @@
+/// imports
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const API_PORT = process.env.API_PORT || 4000;
+const fs = require('fs');
 
+/// their middleware
+app.use(express.urlencoded({extended: true}));
+
+/// my middleware
+const connection = require("./connection.js");
+const globalErrHandler = require("./middleware/errorHandler");
+
+/// db connection
+connection.getConnection((err)=>{
+    if(err) return console.log(err.message);
+    console.log("API Database connection successful.");
+});
+
+/// route handler
+const routeFiles = fs.readdirSync('./routes')
+    .filter(file => ( file.endsWith('.js') && !file.startsWith('_') ));
+for (const file of routeFiles) {
+	const route = require(`./routes/${file}`);
+	app.use(`/${file.slice(0,-3)}`, route);
+};
+// console.log("Routes:", routeFiles);
+
+/// late middleware
+app.use(globalErrHandler);
+
+/// server
+const server = app.listen(API_PORT, () => {
+    console.log(`API started at http://localhost:${server.address().port}/`);
+});
