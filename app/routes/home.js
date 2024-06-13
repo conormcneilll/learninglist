6d413bd2bb2c03f1fc2243746bb9072c8709b2a6
conormcneilll <conormcneill422@gmail.com>
2024-06-13 23:40:54 +0100

    working home.js (BASIC)

--- app/routes/home.js
+++ app/routes/home.js
@@ -4,9 +4,12 @@ const axios = require('axios');
 
 const API_PORT = process.env.API_PORT || 4000;
 
-
 router.get('/', (req, res, next) => {
-  res.send('Home Page');
+  // Attempt to render 'home' EJS template with dynamic data
+  res.render('home', {
+    title: 'Home Page',
+    message: 'This is a message from your backend'
+  });
 });
 
 module.exports = router;
